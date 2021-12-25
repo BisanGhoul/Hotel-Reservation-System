30f6a4fa78ab6b59d6835e8c5391ed6abc10bbd2
BisanGhoul <bbisann@gmail.com>
2021-12-26 00:32:01 +0200

    v1.5 finished cardview for room and its functionalities added icons

--- app/src/main/java/com/example/hotelreservationsystem/Adapters/RoomRecyclerAdapter.java
+++ app/src/main/java/com/example/hotelreservationsystem/Adapters/RoomRecyclerAdapter.java
@@ -42,15 +42,42 @@ public class RoomRecyclerAdapter
     public void onBindViewHolder(ViewHolder holder, int position) {
         final Room room = items.get(position);
         CardView cardView = holder.cardView;
-//        ImageView imageView = (ImageView) cardView.findViewById(R.id.image);
+     //  ImageView imageView = (ImageView) cardView.findViewById(R.id.room_pic);
 //        Glide.with(context).load(room.getImage()).into(imageView);
-        TextView txt = (TextView)cardView.findViewById(R.id.txtName);
-        int x=room.getID();
-        txt.setText(String.valueOf(x));
+        TextView type_txt = (TextView)cardView.findViewById(R.id.roomtype_txt);
+        TextView desc_txt = (TextView)cardView.findViewById(R.id.description_txt);
+        TextView price_txt = (TextView)cardView.findViewById(R.id.price_txt);
+        ImageView ac_img = (ImageView) cardView.findViewById(R.id.ac_img);
+        ImageView tv_img = (ImageView) cardView.findViewById(R.id.tv_img);
+        ImageView wifi_img = (ImageView) cardView.findViewById(R.id.wifi_img);
+        ImageView breakfast_img = (ImageView) cardView.findViewById(R.id.breakfast_img);
 
-        TextView txt2 = (TextView)cardView.findViewById(R.id.txtName2);
-        int y=room.getFloor();
-        txt2.setText(String.valueOf(y));
+        if(room.getType().equals("suite")){
+            type_txt.setText(room.getType());
+            desc_txt.setText("this "+room.getType()+" is ");
+
+        }else{
+            type_txt.setText(room.getType()+ " Room");
+            // TODO: 12/26/2021 capitalize first letter
+            desc_txt.setText("this "+room.getType()+" room is on floor "+room.getFloor()+", & it has "+room.getNumOfBeds()+" beds.");
+        }
+        price_txt.setText(String.valueOf(room.getPrice())+"$");
+
+        if(room.getAC().equals("n")){
+            ac_img.setVisibility(View.GONE);
+        }
+        if(room.getTV().equals("n")){
+            tv_img.setVisibility(View.GONE);
+        }
+        if(room.getWifi().equals("n")){
+            wifi_img.setVisibility(View.GONE);
+        }
+        if(room.getFreeBreakfast().equals("n")){
+            breakfast_img.setVisibility(View.GONE);
+        }
+
+
+
         cardView.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View v){
