c752fa24517fd504b171d660f6b8b09c0ed70e58
BisanGhoul <bbisann@gmail.com>
2021-12-26 13:40:14 +0200

    v1.7 working on layouts

---
+++ app/src/main/java/com/example/hotelreservationsystem/RoomUtilities/RoomSearch.java
@@ -0,0 +1,16 @@
+package com.example.hotelreservationsystem.RoomUtilities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hotelreservationsystem.R;
+
+public class RoomSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_search);
+    }
+}
